ef49c274ed97096d10fb7397b9669d1c39591c59
Adam Skoczylas <skocadam@gmail.com>
2020-02-25 20:27:04 -0500

    emum and tuple

--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
+enum Role { ADMIN, READ_ONLY, AUTHOR}; //enum
+
 const person = {
     name: 'Adam',
     age: 35,
     hobbies: ['coding', 'music', 'biking'],
+    maritalStatus: [2, 'married'],  //Tuple
+    role: Role.ADMIN,
     facts: {
         married: true,
         children: 2,
     }
 };
 
-console.log(person.name);
+if (person.role === Role.ADMIN) {
+    console.log(person.role);
+}
