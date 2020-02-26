e0998cacdd4983f8cfdac278425f2c67b7ecf502
Adam Skoczylas <skocadam@gmail.com>
2020-02-25 20:37:48 -0500

    union types

--- app.ts
+++ app.ts
@@ -1,17 +1,16 @@
-enum Role { ADMIN, READ_ONLY, AUTHOR}; //enum
-
-const person = {
-    name: 'Adam',
-    age: 35,
-    hobbies: ['coding', 'music', 'biking'],
-    maritalStatus: [2, 'married'],  //Tuple
-    role: Role.ADMIN,
-    facts: {
-        married: true,
-        children: 2,
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
     }
-};
-
-if (person.role === Role.ADMIN) {
-    console.log(person.role);
+    return result;
 }
+
+const combineAges = combine(30, 26);
+
+console.log(combineAges)
+
+const combineNames = combine('Max', 'Anna');
+console.log(combineNames);
