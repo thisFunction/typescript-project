028cefea2ff58203205ccae971f35023150c9527
Adam Skoczylas <skocadam@gmail.com>
2020-03-08 20:58:02 -0400

    typescript classes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,58 @@
-const button = document.querySelector('button');
+class Department {
+    private employees: string[] = [];
 
-function clickHandler(message: string) {
-    console.log('clicked!' + message);
+    constructor(private readonly id: number, private name: string) { }
+
+    describe(this: Department) {
+        console.log(`${this.name} department - id ${this.id}`)
+    }
+
+    addEmployee(employee: string) {
+        // validate employee here //
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(`there are ${this.employees.length} employees in ${this.name}`);
+        console.log(`employee names: ${this.employees}`);
+    }
 }
 
-// this will not be included in Js files
-if (button) {
-    let user = "Adam";
-    button.addEventListener('click', clickHandler.bind(null, "Hello"));
+const accounting = new Department(32, 'Accounting');
+accounting.describe();
+accounting.addEmployee('Adam');
+accounting.addEmployee('Maya');
+accounting.printEmployeeInformation();
+
+class ITDepartment extends Department {
+    constructor(id: number, public admins: string[]) {
+        super(id, 'IT');
+    }
 }
+
+const it = new ITDepartment(12, ['Max']);
+it.describe();
+it.addEmployee('Frank');
+it.addEmployee('Laura');
+it.printEmployeeInformation();
+
+class MarketingDepartment extends Department {
+    private reports: string[] = [];
+
+    constructor() {
+        super(63, 'Marketing');
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    printReports() {
+        console.log(`Marketing Reports: ${this.reports}`);
+    }
+}
+
+const marketing = new MarketingDepartment();
+
+marketing.addReport('Analytics');
+marketing.printReports();
